ae55e918b5e7992782937ab6c7f286781e5cc383
Sean Condon <sean.condon@intel.com>
2024-12-22 20:09:13 +0000

    fix lint errors
    Signed-off-by: Sean Condon <sean.condon@intel.com>

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import argparse
-from sys import exception
 
 import pandas as pd
-import requests
 from datetime import datetime, timedelta
 from loguru import logger
 from pandas import DataFrame
@@ -19,13 +17,16 @@ def read_csv(file_path):
 
 def extract_import_entries(data: DataFrame, days: int = 7):
     try:
-        print(data.info())
         one_week_ago = datetime.now() - timedelta(days=days)
         data['date'] = pd.to_datetime(data['Read Date and End Time'])
         last_week_data = data[data['date'] >= one_week_ago]
-        last_week_data = last_week_data.drop(columns=['MPRN','Meter Serial Number', 'Read Date and End Time'])
-        last_week_data = last_week_data.pivot_table(index='date', values='Read Value', columns=['Read Type'], aggfunc='sum')
-        print(last_week_data.info())
+        last_week_data = last_week_data.drop(
+            columns=['MPRN', 'Meter Serial Number', 'Read Date and End Time'])
+        last_week_data = last_week_data.pivot_table(
+            index='date',
+            values='Read Value',
+            columns=['Read Type'],
+            aggfunc='sum')
         return last_week_data
     except Exception as e:
         logger.error(f"Error extracting last week entries: {e}")
@@ -35,12 +36,12 @@ def extract_import_entries(data: DataFrame, days: int = 7):
 def send_to_google_form(data, form_url):
     try:
         for index, row in data.iterrows():
-            exp_kwh: float = row["24 Hr Active Export Register (kWh)"]
+            # exp_kwh: float = row["24 Hr Active Export Register (kWh)"]
             night_kwh = row["Night Import Register (kWh)"]
             day_kwh = row["Day Off-Peak Import Register (kWh)"]
             peak_kwh = row["Day Peak Import Register (kWh)"]
             imp_total = round(night_kwh + day_kwh + peak_kwh, 3)
-            logger.info(f'Processing {index}')
+            logger.info(f'Processing {index} Import:{imp_total}')
             # response = requests.post(form_url, data=row.to_dict())
             # if response.status_code != 200:
             #     logger.error(f"Error sending data to Google: {response.status_code}")
@@ -55,11 +56,10 @@ def main(file1: str, file2: str, form_url):
 
     logger.info("Script started")
 
-
     data1 = read_csv(file1)
     data2 = read_csv(file2)
     if data1.shape[1] > data2.shape[1]:
-      logger.error("Data1 does not match columns of Data2")
+        logger.error("Data1 does not match columns of Data2")
 
     data = pd.concat([data1, data2.iloc[1:]])
 
@@ -69,12 +69,10 @@ def main(file1: str, file2: str, form_url):
         if last_week_data is not None:
             send_to_google_form(last_week_data, form_url)
 
-
     logger.info("Script finished")
 
 
 if __name__ == "__main__":
-
     """Add command line parameters to specify the csv files and the Google Form URL."""
     parser = argparse.ArgumentParser(description="Process CSV files and send data to Google Form.")
     parser.add_argument('csvfile1', type=str, help='Path to the first CSV file')
--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from main import main
 
 
 def test_sample():
-  assert 1 == 1
+    assert 1 == 1
 
 
 def test_main():
-  main('test/HDF_DailyDNP_kWh_01234567890_20-12-2024.csv',
-       'test/HDF_Daily_kWh_01234567890_20-12-2024.csv',
-       'https://docs.google.com/forms/d/e/your-form-id/formResponse')
+    main('test/HDF_DailyDNP_kWh_01234567890_20-12-2024.csv',
+         'test/HDF_Daily_kWh_01234567890_20-12-2024.csv',
+         'https://docs.google.com/forms/d/e/your-form-id/formResponse')
