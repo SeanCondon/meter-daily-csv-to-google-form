0470eecc6d236276c4510ee38d51c4d28ef729b8
Sean Condon <sean.condon@intel.com>
2024-12-28 11:07:29 +0000

    adding more tests for diff
    Signed-off-by: Sean Condon <sean.condon@intel.com>

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from main import main, concatenate_files
+import numpy as np
+
+from main import main, concatenate_files, extract_import_entries, add_diff_columns
 
 MPRN = 'MPRN'
 METER_SERIAL_NUMBER = 'Meter Serial Number'
@@ -131,3 +133,66 @@ def test_concatenate_3_file():
     assert export_row_18th[READ_TYPE] == TWENTYFOUR_HR_ACTIVE_EXPORT_REGISTER
     assert export_row_18th[READ_DATE_AND_END_TIME] == '18-12-2024 00:00'
     assert export_row_18th[READ_VALUE] == 5448.502
+
+
+def test_extract_import_entries_3_file():
+    """
+    Load 3 files and test their pivoting in a table
+    """
+
+    df = concatenate_files(['test/HDF_DailyDNP_kWh_01234567890_20-12-2024.csv',
+                            'test/HDF_Daily_kWh_01234567890_20-12-2024.csv',
+                            'test/HDF_Daily_kWh_01234567890_26-12-2024.csv'])
+
+    assert df is not None
+
+    assert df.shape[0] == 3376
+    assert df.shape[1] == 5
+
+    reduced_df = extract_import_entries(df, 10000)
+    assert reduced_df is not None
+
+    assert reduced_df.shape[0] == 366  # One for every day in year
+    assert reduced_df.shape[1] == 15  # Is 3 years x 5 columns
+
+    # Check that 17th still exists
+    assert reduced_df.index[351] == "12-17"
+    assert reduced_df[TWENTYFOUR_HR_ACTIVE_IMPORT_REGISTER, 2024][351] == 34348.839
+    assert reduced_df[TWENTYFOUR_HR_ACTIVE_EXPORT_REGISTER, 2024][351] == 5448.502
+
+    assert reduced_df.index[352] == "12-18"
+    # Check that 18th import is overridden by bigger value
+    assert reduced_df[TWENTYFOUR_HR_ACTIVE_IMPORT_REGISTER, 2024][352] == 34400
+    # Check that smaller value of export does not override bigger
+    assert reduced_df[TWENTYFOUR_HR_ACTIVE_EXPORT_REGISTER, 2024][352] == 5448.502
+
+    assert reduced_df.index[354] == "12-20"
+    # Check that 20th is added properly
+    assert reduced_df[TWENTYFOUR_HR_ACTIVE_IMPORT_REGISTER, 2024][354] == 34500.640
+    assert reduced_df[TWENTYFOUR_HR_ACTIVE_EXPORT_REGISTER, 2024][354] == 5448.503
+    # Check that night value for 19th does not exist
+    assert np.isnan(reduced_df[NIGHT_IMPORT_REGISTER, 2024][354])
+
+
+def test_add_diff_columns():
+    df = concatenate_files(['test/HDF_DailyDNP_kWh_01234567890_20-12-2024.csv',
+                            'test/HDF_Daily_kWh_01234567890_20-12-2024.csv',
+                            'test/HDF_Daily_kWh_01234567890_26-12-2024.csv'])
+
+    reduced_df = extract_import_entries(df, 10000)
+
+    with_diff_cols = add_diff_columns(reduced_df)
+    assert with_diff_cols is not None
+
+    assert with_diff_cols.shape[0] == 366  # One for every day in year
+    assert with_diff_cols.shape[1] == 30  # Is 3 years x 5 columns x 2
+
+    # Check 17th value
+    assert with_diff_cols.index[351] == "12-17"
+    assert with_diff_cols[TWENTYFOUR_HR_ACTIVE_IMPORT_REGISTER, 2024][351] == 34348.839
+    assert with_diff_cols[TWENTYFOUR_HR_ACTIVE_EXPORT_REGISTER, 2024][351] == 5448.502
+
+    assert with_diff_cols.index[352] == "12-18"
+    assert with_diff_cols[TWENTYFOUR_HR_ACTIVE_IMPORT_REGISTER, 2024][352] == 34400
+    assert with_diff_cols[TWENTYFOUR_HR_ACTIVE_EXPORT_REGISTER, 2024][352] == 5448.502
+    assert with_diff_cols[TWENTYFOUR_HR_ACTIVE_IMPORT_REGISTER+'_diff', 2024][352] == (34400 - 34348.839)
